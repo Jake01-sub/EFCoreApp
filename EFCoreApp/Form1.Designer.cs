743fe0d8c348154fa82319b495cd03164e8fa754
Jake01-sub <l.buchan.476132@umindanao.edu.ph>
2026-04-15 15:28:02 +0800

    Day 2

--- EFCoreApp/Form1.Designer.cs
+++ EFCoreApp/Form1.Designer.cs
@@ -29,6 +29,17 @@
         private void InitializeComponent()
         {
             dataGridView1 = new DataGridView();
+            txtSearch = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            txtName = new TextBox();
+            label3 = new Label();
+            txtDescription = new TextBox();
+            btnAdd = new Button();
+            btnUpdate = new Button();
+            btnDelete = new Button();
+            label4 = new Label();
+            txtId = new TextBox();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
@@ -36,16 +47,132 @@
             // 
             dataGridView1.Anchor = AnchorStyles.None;
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(12, 12);
+            dataGridView1.Location = new Point(544, 65);
             dataGridView1.Name = "dataGridView1";
-            dataGridView1.Size = new Size(776, 409);
+            dataGridView1.Size = new Size(636, 449);
             dataGridView1.TabIndex = 0;
+            dataGridView1.CellClick += dataGridView1_CellClick;
+            // 
+            // txtSearch
+            // 
+            txtSearch.Anchor = AnchorStyles.None;
+            txtSearch.Location = new Point(850, 27);
+            txtSearch.Name = "txtSearch";
+            txtSearch.Size = new Size(330, 23);
+            txtSearch.TabIndex = 1;
+            txtSearch.TextChanged += txtSearch_TextChanged;
+            // 
+            // label1
+            // 
+            label1.Anchor = AnchorStyles.None;
+            label1.AutoSize = true;
+            label1.Location = new Point(786, 31);
+            label1.Name = "label1";
+            label1.Size = new Size(42, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Search";
+            // 
+            // label2
+            // 
+            label2.Anchor = AnchorStyles.None;
+            label2.AutoSize = true;
+            label2.Location = new Point(24, 121);
+            label2.Name = "label2";
+            label2.Size = new Size(39, 15);
+            label2.TabIndex = 4;
+            label2.Text = "Name";
+            // 
+            // txtName
+            // 
+            txtName.Anchor = AnchorStyles.None;
+            txtName.Location = new Point(24, 139);
+            txtName.Name = "txtName";
+            txtName.Size = new Size(284, 23);
+            txtName.TabIndex = 3;
+            // 
+            // label3
+            // 
+            label3.Anchor = AnchorStyles.None;
+            label3.AutoSize = true;
+            label3.Location = new Point(24, 174);
+            label3.Name = "label3";
+            label3.Size = new Size(67, 15);
+            label3.TabIndex = 6;
+            label3.Text = "Description";
+            // 
+            // txtDescription
+            // 
+            txtDescription.Anchor = AnchorStyles.None;
+            txtDescription.Location = new Point(24, 192);
+            txtDescription.Name = "txtDescription";
+            txtDescription.Size = new Size(284, 23);
+            txtDescription.TabIndex = 5;
+            // 
+            // btnAdd
+            // 
+            btnAdd.Location = new Point(24, 238);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(284, 27);
+            btnAdd.TabIndex = 7;
+            btnAdd.Text = "Add";
+            btnAdd.UseVisualStyleBackColor = true;
+            btnAdd.Click += btnAdd_Click;
+            // 
+            // btnUpdate
+            // 
+            btnUpdate.Location = new Point(24, 271);
+            btnUpdate.Name = "btnUpdate";
+            btnUpdate.Size = new Size(284, 27);
+            btnUpdate.TabIndex = 8;
+            btnUpdate.Text = "Update";
+            btnUpdate.UseVisualStyleBackColor = true;
+            btnUpdate.Click += btnUpdate_Click;
+            // 
+            // btnDelete
+            // 
+            btnDelete.Location = new Point(24, 304);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(284, 27);
+            btnDelete.TabIndex = 9;
+            btnDelete.Text = "Delete";
+            btnDelete.UseVisualStyleBackColor = true;
+            btnDelete.Click += btnDelete_Click;
+            // 
+            // label4
+            // 
+            label4.Anchor = AnchorStyles.None;
+            label4.AutoSize = true;
+            label4.Location = new Point(24, 65);
+            label4.Name = "label4";
+            label4.Size = new Size(18, 15);
+            label4.TabIndex = 11;
+            label4.Text = "ID";
+            // 
+            // txtId
+            // 
+            txtId.Anchor = AnchorStyles.None;
+            txtId.Location = new Point(24, 83);
+            txtId.Name = "txtId";
+            txtId.Size = new Size(284, 23);
+            txtId.TabIndex = 10;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            BackColor = SystemColors.ActiveCaption;
+            ClientSize = new Size(1223, 537);
+            Controls.Add(label4);
+            Controls.Add(txtId);
+            Controls.Add(btnDelete);
+            Controls.Add(btnUpdate);
+            Controls.Add(btnAdd);
+            Controls.Add(label3);
+            Controls.Add(txtDescription);
+            Controls.Add(label2);
+            Controls.Add(txtName);
+            Controls.Add(label1);
+            Controls.Add(txtSearch);
             Controls.Add(dataGridView1);
             Name = "Form1";
             StartPosition = FormStartPosition.CenterScreen;
@@ -53,10 +180,22 @@
             Load += Form1_Load;
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private DataGridView dataGridView1;
+        private TextBox txtSearch;
+        private Label label1;
+        private Label label2;
+        private TextBox txtName;
+        private Label label3;
+        private TextBox txtDescription;
+        private Button btnAdd;
+        private Button btnUpdate;
+        private Button btnDelete;
+        private Label label4;
+        private TextBox txtId;
     }
 }
--- EFCoreApp/Form1.cs
+++ EFCoreApp/Form1.cs
@@ -1,3 +1,4 @@
+using EFCoreDomain.Models;
 using EFCoreInfrastructure.Data;
 
 namespace EFCoreApp
@@ -11,10 +12,107 @@ namespace EFCoreApp
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            using (var dbContext = new AppDbContext())
+            GetCategories();
+        }
+
+        // CRUD - Read (List)
+        private void GetCategories()
+        {
+            using var dbContext = new AppDbContext();
+            dataGridView1.DataSource = dbContext.Categories.OrderByDescending(c => c.Name).ToList();
+        }
+
+        // CRUD - Read (List/Single Entity)
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+            using var dbContext = new AppDbContext();
+
+            if (string.IsNullOrEmpty(txtSearch.Text.Trim()))
             {
                 dataGridView1.DataSource = dbContext.Categories.ToList();
+                return;
             }
+
+            dataGridView1.DataSource = dbContext.Categories.Where(c => c.Name
+                                             .ToLower()
+                                             .Contains(txtSearch.Text.ToLower().Trim()))
+                                             .ToList();
+        }
+
+        // CRUD - Create
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            using var dbContext = new AppDbContext();
+            var category = new Category();
+            category.Name = txtName.Text.Trim();
+            category.Description = txtDescription.Text.Trim();
+
+            dbContext.Categories.Add(category);
+            dbContext.SaveChanges();
+
+            MessageBox.Show("Category added successfully!");
+            GetCategories();
+        }
+
+        // CRUD - Update
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            using var dbContext = new AppDbContext();
+            if (string.IsNullOrEmpty(txtId.Text.Trim()))
+            {
+                MessageBox.Show("Please enter a valid category ID.");
+                return;
+            }
+
+            Guid categoryId = Guid.Parse(txtId.Text.Trim());
+            var category = dbContext.Categories.Find(categoryId);
+            category.Name = txtName.Text.Trim();
+            category.Description = txtDescription.Text.Trim();
+
+            dbContext.Categories.Update(category);
+            dbContext.SaveChanges();
+            MessageBox.Show("Category updated successfully!");
+            GetCategories();
+        }
+
+        // CRUD - Delete
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            using var dbContext = new AppDbContext();
+
+            if (string.IsNullOrEmpty(txtId.Text.Trim()))
+            {
+                MessageBox.Show("Please enter a valid category ID.");
+                return;
+            }
+
+            Guid categoryId = Guid.Parse(txtId.Text.Trim());
+            var category = dbContext.Categories.Find(categoryId);
+
+
+            if (category == null)
+            {
+                MessageBox.Show("Category not found.");
+            }
+
+            dbContext.Categories.Remove(category);
+            dbContext.SaveChanges();
+
+            MessageBox.Show("Category deleted successfully!");
+            GetCategories();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            { 
+                var selectedRow = dataGridView1.Rows[e.RowIndex];
+                var description = selectedRow.Cells["Description"].Value?.ToString();
+                txtId.Text = selectedRow.Cells["Id"].Value?.ToString();
+                txtName.Text = selectedRow.Cells["Name"].Value?.ToString();
+                txtDescription.Text = selectedRow.Cells["Description"].Value?.ToString() ?? "{Needs Updating}";
+            }
+
         }
     }
 }
